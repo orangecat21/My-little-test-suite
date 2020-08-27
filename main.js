eb70e5554aab16f8de937ebd366d2efda2792ba7
orangefruit21 <orangecat21@yandex.ru>
2020-08-27 12:20:03 +0300

    Add report method in TestSuite

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ import TestSuite from '../testSuit/testSuit.js';
 
 const tester = new TestSuite();
 const assert = tester.assert;
+const report = tester.report;
 
-
-assert(true, 'Outside call!');
--- testSuit/testSuit.js
+++ testSuit/testSuit.js
@@ -1,20 +1,22 @@
 class TestSuite {
     constructor() {
-        const result = document.createElement('ul');
-        result.className = 'result';
-        document.body.appendChild(result);
-
-        this.assert = (value, descr) => {
-            let li = document.createElement('li');
-            li.className = value ? 'pass' : 'fail';
-            li.innerText = descr;
-            result.appendChild(li);
-        }
-    
+        this.result = document.createElement('ul');
+        this.result.className = 'result';
+        document.body.appendChild(this.result);
+        
         this.assert(false, `Check tester.`);
         this.assert(true, `Test suit is running.`);
-    
-    
+    }
+
+    assert = (value, descr) => {
+        let li = document.createElement('li');
+        li.className = value ? 'pass' : 'fail';
+        li.innerText = descr;
+        this.result.appendChild(li);
+    }
+
+    report = (msg) => {
+        this.assert(true, msg);
     }
 };
 
